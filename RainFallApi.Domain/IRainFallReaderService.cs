043af709d372b096a014ab66ac0392105aad427c
Ron Natividad <rnatividad@marpoint.gr>
2024-01-05 04:23:21 +0800

    Added RainFallReader Interface

---
+++ RainFallApi.Domain/IRainFallReaderService.cs
@@ -0,0 +1,9 @@
+﻿using RainFallApi.Domain.Models;
+
+namespace RainFallApi.Domain
+{
+    public interface IRainFallReaderService
+    {
+        Task<HttpResponseMessage> GetRainfallReadings(string stationId, int count = 10);
+    }
+}
